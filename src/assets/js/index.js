c25a0e9aab3bea21282c307440da891696b314ec
Бун Владимир <qwelp@mail.ru>
2024-10-24 11:52:05 +0300

    v0.1

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,6 @@
 import '../css/index.css';
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
 
 function sliderHero() {
 	const sliderHero = document.querySelector('.js-sliders-hero-banners');
@@ -16,37 +16,163 @@ function sliderHero() {
 	}
 }
 
-function productsSlider() {
-	const slider = document.querySelector('.js-products-tabs__slider');
-	const navLeft = slider.querySelector('.js-products-tabs__nav-left');
-	const navRight = slider.querySelector('.js-products-tabs__nav-right');
+function sliderHeroMobile() {
+	const slider = document.querySelector('.js-slider-menu-brands-mobile');
 
 	if (slider) {
+		const scrollbar = slider.querySelector('.swiper-scrollbar');
+
 		new Swiper(slider, {
-			modules: [Navigation],
+			modules: [Scrollbar],
 			loop: true,
-			slidesPerView: '4',
-			spaceBetween: 40,
-			navigation: {
-				nextEl: navLeft,
-				prevEl: navRight
-			},
+			slidesPerView: 'auto',
+			spaceBetween: 8,
+			scrollbar: {
+				el: scrollbar,
+			}
 		});
 	}
 }
 
+function popularCategories() {
+	const slider = document.querySelector('.js-slider-popular-categories');
 
-function brandsSlider() {
-	const slider = document.querySelector('.js-brands__slider');
-	const navLeft = slider.querySelector('.js-brands__nav-left');
-	const navRight = slider.querySelector('.js-brands__nav-right');
+	if (slider) {
+		new Swiper(slider, {
+			loop: true,
+			slidesPerView: 'auto',
+			spaceBetween: 16,
+			breakpoints: {
+				640: {
+					slidesPerView: 5,
+					spaceBetween: 16
+				},
+				1280: {
+					slidesPerView: 6,
+					spaceBetween: 32
+				}
+			}
+		});
+	}
+}
+
+function productsSlider() {
+	const slider = document.querySelector('.js-products-tabs__slider');
+
+	if (slider) {
+		const navLeft = slider.querySelector('.js-products-tabs__nav-left');
+		const navRight = slider.querySelector('.js-products-tabs__nav-right');
+
+		new Swiper(slider, {
+			modules: [Navigation],
+			loop: true,
+			slidesPerView: '2',
+			spaceBetween: 16,
+			navigation: {
+				nextEl: navLeft,
+				prevEl: navRight
+			},
+			breakpoints: {
+				640: {
+					slidesPerView: 4,
+					spaceBetween: 16
+				},
+				1280: {
+					slidesPerView: 4,
+					spaceBetween: 32
+				},
+				1536: {
+					slidesPerView: 4,
+					spaceBetween: 40
+				}
+			}
+		});
+	}
+}
+
+function compilationsSlider() {
+	const slider = document.querySelector('.js-compilations__slider');
 
 	if (slider) {
 		new Swiper(slider, {
 			modules: [Navigation],
 			loop: true,
-			slidesPerView: '6',
-			spaceBetween: 40,
+			slidesPerView: 'auto',
+			spaceBetween: 8,
+			breakpoints: {
+				640: {
+					slidesPerView: 4,
+					spaceBetween: 16
+				},
+				1280: {
+					slidesPerView: 4,
+					spaceBetween: 32
+				},
+				1536: {
+					slidesPerView: 4,
+					spaceBetween: 40
+				}
+			}
+		});
+	}
+}
+
+function brandsSlider() {
+	const slider = document.querySelector('.js-brands__slider');
+
+	if (slider) {
+		const navLeft = slider.querySelector('.js-brands__nav-left');
+		const navRight = slider.querySelector('.js-brands__nav-right');
+
+		new Swiper(slider, {
+			modules: [Navigation],
+			loop: true,
+			slidesPerView: 'auto',
+			spaceBetween: 8,
+			navigation: {
+				nextEl: navLeft,
+				prevEl: navRight
+			},
+			breakpoints: {
+				640: {
+					slidesPerView: 6,
+					spaceBetween: 20
+				},
+				1280: {
+					slidesPerView: 6,
+					spaceBetween: 40
+				}
+			}
+		});
+	}
+}
+
+function eventsSlider() {
+	const slider = document.querySelector('.js-events__slider');
+
+	if (slider) {
+		const navLeft = slider.querySelector('.js-events__nav-left');
+		const navRight = slider.querySelector('.js-events__nav-right');
+
+		new Swiper(slider, {
+			modules: [Navigation],
+			loop: true,
+			slidesPerView: 1,
+			spaceBetween: 10,
+			breakpoints: {
+				640: {
+					slidesPerView: 4,
+					spaceBetween: 16
+				},
+				1280: {
+					slidesPerView: 3,
+					spaceBetween: 32
+				},
+				1536: {
+					slidesPerView: 4,
+					spaceBetween: 40
+				}
+			},
 			navigation: {
 				nextEl: navLeft,
 				prevEl: navRight
@@ -57,6 +183,10 @@ function brandsSlider() {
 
 document.addEventListener('DOMContentLoaded', () => {
 	sliderHero();
+	sliderHeroMobile();
+	popularCategories();
 	productsSlider();
+	compilationsSlider();
 	brandsSlider();
+	eventsSlider();
 });
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,6 +6,110 @@ import MobileMenu from './classes/MobileMenu.js';
 import Dropdown from './classes/Dropdown.js';
 import 'virtual:svg-icons-register';
 
+function getElementPosition(element) {
+	let top = 0;
+	let left = 0;
+
+	while (element) {
+		top += element.offsetTop;
+		left += element.offsetLeft;
+		element = element.offsetParent;
+	}
+
+	return { top, left };
+}
+
+function menuCatalogAutoTopStyle() {
+	const viewportWidth = window.innerWidth;
+	const toggleButton = document.querySelector('.js-toggle-menu');
+	const headerWrapper = document.querySelector('.js-header-wrapper');
+	const popupMenu = document.querySelector('.popup-menu');
+
+	const positionToggleMenu = getElementPosition(toggleButton);
+
+	if (viewportWidth >= 1536) {
+		popupMenu.style.left = `${positionToggleMenu.left}px`;
+	} else {
+		popupMenu.removeAttribute('style');
+	}
+
+	if (viewportWidth >= 768) {
+		popupMenu.style.top = `${headerWrapper.offsetHeight + 10}px`;
+	}
+}
+
+function menuCatalogToggle() {
+	const button = document.querySelector('.js-toggle-menu');
+	const menu = document.querySelector('.popup-menu');
+	const popupWrapper = document.querySelector('.js-popup-wrapper');
+	const closeButton = document.querySelector('.js-popup-close'); // Находим кнопку закрытия
+	const openButton = document.querySelector('.js-popup-open'); // Находим кнопку открытия
+
+	if (button) {
+		button.addEventListener('click', () => {
+			// Открываем или закрываем меню и обёртку
+			toggleMenu();
+		});
+	}
+
+	// Закрываем меню при клике на обертку popupWrapper
+	if (popupWrapper) {
+		popupWrapper.addEventListener('click', () => {
+			if (!menu.classList.contains('hidden')) {
+				toggleMenu();
+			}
+		});
+	}
+
+	// Закрываем меню при клике на кнопку закрытия
+	if (closeButton) {
+		closeButton.addEventListener('click', () => {
+			if (!menu.classList.contains('hidden')) {
+				toggleMenu();
+			}
+		});
+	}
+
+	// Открываем меню при клике на кнопку открытия
+	if (openButton) {
+		openButton.addEventListener('click', () => {
+			if (menu.classList.contains('hidden')) {
+				menu.classList.remove('hidden');
+				popupWrapper.classList.remove('hidden');
+			}
+		});
+	}
+
+	// Функция для переключения видимости меню
+	function toggleMenu() {
+		if (menu.classList.contains('hidden')) {
+			menu.classList.remove('hidden');
+			popupWrapper.classList.remove('hidden');
+		} else {
+			menu.classList.add('hidden');
+			popupWrapper.classList.add('hidden');
+		}
+	}
+}
+
+function popupWrapperTopStyleAuto() {
+	const headerWrapper = document.querySelector('.js-header-wrapper');
+	const popupWrapper = document.querySelector('.js-popup-wrapper');
+	const footerWrapper = document.querySelector('.js-footer');
+
+	// Устанавливаем отступ сверху в зависимости от высоты хедера
+	popupWrapper.style.top = `${headerWrapper.offsetHeight}px`;
+
+	// Рассчитываем полную высоту страницы и высоту видимой области
+	const availableHeight = Math.max(document.documentElement.scrollHeight, window.innerHeight);
+
+	// Вычитаем высоту хедера и футера из полной высоты
+	popupWrapper.style.height = `${availableHeight - headerWrapper.offsetHeight - footerWrapper.offsetHeight}px`;
+}
+
+
+window.addEventListener('resize', menuCatalogAutoTopStyle);
+
 document.addEventListener('DOMContentLoaded', () => {
 	new MobileMenu();
 
@@ -15,6 +119,8 @@ document.addEventListener('DOMContentLoaded', () => {
 			new Dropdown(dropdown);
 		});
 	}
-});
 
-console.log('main.js');
+	menuCatalogAutoTopStyle();
+	popupWrapperTopStyleAuto();
+	menuCatalogToggle();
+});
